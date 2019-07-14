857965830c88a8103a1ea797c8c7ae7056c717fc
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-14 19:52:09 +0800

    Add return for status 400

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,7 +33,9 @@ router.post(
       // See if user exists
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Get user gravatar
