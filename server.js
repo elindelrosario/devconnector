0b549ac9cf64225b3c5016b8828fd912117cecee
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-14 18:48:49 +0800

    Add sample request and body validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running!"));
 
 // Define routes
