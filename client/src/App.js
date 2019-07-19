e3a8296821d65f7b7139cbb8fbcd85102ea5a3cc
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-19 16:37:37 +0800

    Alert component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Alert from "./components/layout/Alert";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
@@ -18,6 +19,7 @@ const App = () => (
         <NavBar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
