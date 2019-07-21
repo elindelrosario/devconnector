08ac5b26e7733be3b6690edc3a02d561b043976d
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-21 11:31:30 +0800

    Display profiles

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -42,6 +42,7 @@ export default function(state = initialState, action) {
       return {
         ...state,
         profile: null,
+        profiles: [],
         repos: [],
         loading: false,
         error: null
