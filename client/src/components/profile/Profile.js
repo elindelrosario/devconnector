ce251bd0966c735cfd106b53610aad36f03481ea
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-21 12:34:03 +0800

    Profile component

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,53 @@
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfileById } from "../../actions/profile";
+
+const Profile = ({
+  match,
+  getProfileById,
+  profile: { profile, loading },
+  auth
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.user_id);
+  }, [getProfileById]);
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Developers
+          </Link>
+          {auth.isAuthenticated &&
+            !loading &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-primary">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfileById }
+)(Profile);
