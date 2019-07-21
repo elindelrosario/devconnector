5fa6452fe545a66f03f3db73b4f7c8d75f503758
Elin del Rosario <elindelrosario@Elin-Andreana.local>
2019-07-21 19:28:48 +0800

    Change expiresIn to 1 hour

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -62,7 +62,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecretKey"),
-        { expiresIn: 360000 },
+        { expiresIn: 3600 },
         (error, token) => {
           if (error) throw error;
           res.json({ token });
